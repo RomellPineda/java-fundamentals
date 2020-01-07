523ffb26d8848b6329cf308d7fd372b634d030c3
Romell Pineda <romell.pineda@gmail.com>
2020-01-07 12:31:02 -0800

    out for lunch

--- basics/Main.java
+++ basics/Main.java
@@ -1,5 +1,43 @@
+import java.time.LocalDateTime;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("We out here!");
+    System.out.println("lab 01");
+
+    System.out.println(pluralize("wurd", 0));
+    flipNHeads(2);
+    clock();
+  }
+
+  public static String pluralize(String word, int num) {
+    if (num == 0 || num > 1) {
+      String yld = word + "s";
+      return yld;
+    }
+    return word;
+  }
+
+  public static void flipNHeads(int n) {
+    int headcount = 0;
+    int flips = 0;
+    while (headcount != n) {
+      double coin = Math.random();
+      boolean heads = coin > 0.5;
+      flips++;
+      String face = heads ? "heads" : "tails";
+      if (heads) {
+        headcount++;
+      } else if (headcount > 0) {
+        headcount--;
+      }
+      System.out.println(face);
+    }
+    String yld = "it took " + flips + " to flip " + n + " in a row";
+    System.out.println(yld);
+  }
+
+  public static void clock() {
+    LocalDateTime now = LocalDateTime.now();
+    System.out.println(now);
   }
 }
