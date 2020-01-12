1b12f034b58dd744ede39fa1d16e3e1c5ba3b1ae
Romell Pineda <romell.pineda@gmail.com>
2020-01-12 02:37:03 -0800

    stage linter

--- basiclibrary/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/src/test/java/basiclibrary/LibraryTest.java
@@ -4,6 +4,10 @@
 package basiclibrary;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class LibraryTest {
@@ -35,8 +39,41 @@ public class LibraryTest {
         int[] testArray = {1, 2, 3, 3};
         int[] testArrayTwo = {5, 4, 3, 2, 1, 0};
 
-        assertEquals(true, Library.containsDuplicates(testArray));
-        assertEquals(false, Library.containsDuplicates(testArrayTwo));
+        assertTrue(Library.containsDuplicates(testArray));
+        assertFalse(Library.containsDuplicates(testArrayTwo));
+    }
+
+    @Test public void testAverage() {
+        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        double control = 5.5;
+        assertEquals(control, Library.average(testArray), .1);
+    }
+
+    @Test public void testLowestAverage() {
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57},
+                {-4, 3, 3, 5, 0, -2, -4}
+        };
+        int[] control = {-4, 3, 3, 5, 0, -2, -4};
+        assertArrayEquals(control, Library.lowestAverage(weeklyMonthTemperatures));
+    }
+
+    @Test public void testTally() {
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+
+        assertEquals("Bush", Library.tally(votes));
     }
 
 }
